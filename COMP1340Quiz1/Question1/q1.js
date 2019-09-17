96e44fce9af85fc19b863ed62391764a28735c4d
Peach <peach@peachs-mbp.wifi.bcit.ca>
2019-09-17 14:11:41 -0700

    first commit

---
+++ COMP1340Quiz1/Question1/q1.js
@@ -0,0 +1,6 @@
+//Github is a distributed version control system that allows multiple developers 
+contribute to a piece of work/code in a repository. That repository
+can be accessed both online on the github website as well as in a local 
+repository on the developer's computer. Meanwhile, git is a program that 
+that allows a developer to upload code from their local repository online. 
+
